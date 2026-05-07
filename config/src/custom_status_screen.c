5ade9c905bfdbb9ee3fbd3d101f23f576a1e98e2
Greverf <brayergreverf@gmail.com>
2026-05-07 03:16:56 -0500

    corrigiendo oled21

--- config/src/custom_status_screen.c
+++ config/src/custom_status_screen.c
@@ -1,2 +1,9 @@
 //nombre de archivo custom_status_screen.c carpeta src
 
+#include "custom_status_screen.h"
+
+lv_obj_t *zmk_display_status_screen() {
+    lv_obj_t *screen = lv_obj_create(NULL);
+    // No hacer NADA más
+    return screen;
+}
