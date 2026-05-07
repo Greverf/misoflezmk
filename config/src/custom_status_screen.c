96bd5143e945f4376f08f921e06b4b746a6666e1
Greverf <brayergreverf@gmail.com>
2026-05-07 03:02:25 -0500

    corrigiendo oled19

--- config/src/custom_status_screen.c
+++ config/src/custom_status_screen.c
@@ -1,25 +1,22 @@
+//nombre de archivo custom_status_screen.c carpeta src
+
 #include "custom_status_screen.h"
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 lv_obj_t *zmk_display_status_screen() {
-    // Crear la pantalla
     lv_obj_t *screen = lv_obj_create(NULL);
     
     // Poner fondo negro
     lv_obj_set_style_bg_color(screen, lv_color_black(), 0);
+    lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, 0);
     
-    // Crear una etiqueta (label) con texto
+    // Crear el texto
     lv_obj_t *label = lv_label_create(screen);
     lv_label_set_text(label, "HOLA MUNDO!");
-    
-    // Poner el texto en blanco
     lv_obj_set_style_text_color(label, lv_color_white(), 0);
-    
-    // Centrar el texto en la pantalla
     lv_obj_center(label);
     
-    // Devolver la pantalla
     return screen;
 }
--- config/src/custom_status_screen.h
+++ config/src/custom_status_screen.h
@@ -1,25 +1,7 @@
-#include "custom_status_screen.h"
-#include <zephyr/logging/log.h>
+//nombre de archivo custom_status_screen.h carpeta src
 
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+#pragma once
 
-lv_obj_t *zmk_display_status_screen() {
-    lv_obj_t *screen = lv_obj_create(NULL);
-    
-    // Limpieza FORZADA de la pantalla completa
-    lv_obj_set_style_bg_color(screen, lv_color_black(), 0);
-    lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, 0);
-    lv_obj_set_style_border_width(screen, 0, 0);
-    
-    // Crear el texto
-    lv_obj_t *label = lv_label_create(screen);
-    lv_label_set_text(label, "HOLA");
-    lv_obj_set_style_text_color(label, lv_color_white(), 0);
-    lv_obj_center(label);
-    
-    // Forzar un refresco inmediato
-    lv_obj_invalidate(screen);
-    lv_refr_now(NULL);
-    
-    return screen;
-}
+#include <lvgl.h>
+
+lv_obj_t *zmk_display_status_screen(void);
